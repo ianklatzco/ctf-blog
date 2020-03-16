3114b66a0c1f9716f49ae91ec407ee3311ffdf6e
Ian Klatzco <iklatzco@gmail.com>
2020-03-15 22:14:53 -0700

    automated

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,1 @@
-bundle exec jekyll build && git add . && git commit -m 'automated' && git push
+git add . && git commit -m 'automated' && git push
---
+++ readme.md
@@ -0,0 +1,2 @@
+jekyll here is built on github's servers, so all you need to do is push to
+the repo and it'll build.
